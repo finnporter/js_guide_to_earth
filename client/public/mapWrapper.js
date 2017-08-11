f8d00cd5e54f842c3f8679d2fb7b08b1d51ee076
Jamie Martin <jamiemartinmusic@gmail.com>
2017-08-11 16:17:58 +0100

    indentation of files fixed

--- client/public/mapWrapper.js
+++ client/public/mapWrapper.js
@@ -2,20 +2,20 @@ var earth;
 var options;
 
 var renderMap = function() {
-  options = { zoom: 5.0, position: [55.9533, 3.1883] };
+  options = { zoom: 2.0, position: [55.9533, 3.1883] };
   earth = new WE.map('earth_div', options); 
   WE.tileLayer("http://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg", {
-            minZoom: 0,
-            maxZoom: 5,
-            attribution: "NASA"
-          }).addTo(earth);
+    minZoom: 0,
+    maxZoom: 5,
+    attribution: "NASA"
+  }).addTo(earth);
   console.log(earth)
   earth.on("click", addMarker)
 }
 
 var addMarker = function(evt) {
   if (evt.latitude !== null && evt.longitude !== null) {
-  var marker = WE.marker([evt.latitude, evt.longitude]).addTo(earth)
-  console.log(evt)
-};
+    var marker = WE.marker([evt.latitude, evt.longitude]).addTo(earth)
+    console.log(evt) //console logs long and lat
+  };
 }
