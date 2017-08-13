56e36509bf3540f3223fae177930eae8f2ae6cc0
finnporter <iamfinnporter@gmail.com>
2017-08-13 11:39:17 +0100

    makes request to countries API

--- client/public/app.js
+++ client/public/app.js
@@ -1,10 +1,29 @@
+var makeCountriesRequest = function(url, callback) {
+  var request = new XMLHttpRequest();
+
+  request.addEventListener('load', callback);
+
+  request.open('GET', url);
+  request.send();
+};
+
+var requestCountriesComplete = function() {
+  if (this.status !== 200) return;
+  var jsonString = this.responseText;
+  var countries = JSON.parse(jsonString);
+  //call function to populate info window here, pass var countries
+};
+
 var app = function() {
  renderMap();
-}
+
+ var url = 'https://restcountries.eu/rest/v2/all';
+ makeCountriesRequest(url, requestCountriesComplete)
+};
 
 
 
 
 
 
-window.addEventListener('load', app)
+window.addEventListener('load', app);
