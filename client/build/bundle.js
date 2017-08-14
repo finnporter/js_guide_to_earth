1f30d9261cfbb3aa01a9377a1b4edb41a86b8202
Faisal <faisal_a@live.co.uk>
2017-08-14 21:21:12 +0100

    country population method refactored to mapWrapper prototype

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -141,19 +141,20 @@ MapWrapper.prototype.addMarker = function(evt) {
   }
 };
 
+MapWrapper.prototype.countryPopulation = function(info) {
+  for (country of this.countriesInfo.name) {
+    if (country === info.formatted_address) {
+      this.countriesInfo.population.find(function(countObject) {
+        return country === countObject.name
+      }).population
+    }
+  }
+};
+
 MapWrapper.prototype.fillInfoWindow = function(info) {
   // console.log(this.countriesInfo.name)
-
-  var countryPopulation = function(info) {
-    for (country of this.countriesInfo.name) {
-      if (country === info.formatted_address) {
-        this.countriesInfo.population.find(function(countObject) {
-          return country === countObject.name
-        }).population
-      }
-    }
-  };
-  console.log(countryPopulation);
+  // var cont = countryPopulation(info);
+  // console.log(cont);
 
   for (country of this.countriesInfo.name) {
     if (country === info.formatted_address) {
