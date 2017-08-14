53e5d385628d4a519a3b8f482e3f5d25d968a293
Faisal <faisal_a@live.co.uk>
2017-08-14 10:02:25 +0100

    code tidied up ready to merge

--- client/public/mapWrapper.js
+++ client/public/mapWrapper.js
@@ -16,8 +16,8 @@ var renderMap = function() {
 var addMarker = function(evt) {
   if (evt.latitude !== null && evt.longitude !== null) {
     var marker = WE.marker([evt.latitude, evt.longitude]).addTo(earth)
-    console.log(evt.latlng) //console logs long and lat
-    countriesSearch(evt)
+    console.log(evt.latlng); 
+    countriesSearch(evt);
   };
 }
 
@@ -25,19 +25,14 @@ var countriesSearch = function(evt) {
   searchCity(evt);
 
   var geocoder = new google.maps.Geocoder;
-  geocoder.geocode({ 'location': evt.latlng}, function(results, status){
+  geocoder.geocode({ 'location': evt.latlng}, function(results, status) {
     var single = results[results.length - 1];
-    console.log(single.formatted_address)
-  // display = results[0].formatted_address;
-  // split = display.split(",")
-  // console.log(split[split.length - 1]);
+    console.log(single.formatted_address);
   });
 }
 
 var searchCity = function(evt) {
-  // console.log(evt);
   var url = "https://api.teleport.org/api/locations/" + evt.latitude + "," + evt.longitude;
-  // var url = "https://api.teleport.org/api/locations/37.77493,-122.41942";
   makeRequest(url, requestComplete);
 }
 
