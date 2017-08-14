a36ea76b7af5ca4033ef4952b50f10b02ad89de8
finnporter <iamfinnporter@gmail.com>
2017-08-14 21:05:04 +0100

    refactors apiProcessing and corresponding lines in mapWrapper

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -87,14 +87,16 @@ var requestCountriesComplete = function() {
   console.log(countries);
 
   var countriesApi = new ApiProcessing(); 
-  
+
   var countriesInfo = {
   name: countriesApi.getCountryNames(countries),
-  area: countriesApi.getCountryArea(countries),
-  population: countriesApi.getCountryPopulation(countries),
-  region: countriesApi.getCountryRegions(countries),
-  border: countriesApi.getCountryBorders(countries)
+  stats: countriesApi.processCountriesApi(countries)
+  // area: countriesApi.getCountryArea(countries),
+  // population: countriesApi.getCountryPopulation(countries),
+  // region: countriesApi.getCountryRegions(countries),
+  // border: countriesApi.getCountryBorders(countries)
 };
+console.log(countriesInfo);
 
   var mainMap = new MapWrapper(countriesInfo);
   mainMap.renderMap();
@@ -209,49 +211,63 @@ ApiProcessing.prototype.getCountryNames = function(countries) {
   return countryNames;
 };
 
-ApiProcessing.prototype.getCountryArea = function(countries) {
-  var countryAreas = [];
-  countryAreas = countries.map(function(country) {
+ApiProcessing.prototype.processCountriesApi = function(countries) {
+  var countryStats = [];
+  countryStats = countries.map(function(country) {
     var reformattedCountry = {};
     reformattedCountry.name = country.name;
     reformattedCountry.area = country.area;
-    return reformattedCountry;
-  });
-  return countryAreas;
-};
-
-ApiProcessing.prototype.getCountryPopulation = function(countries) {
-  var countryPopulations = [];
-  countryPopulations = countries.map(function(country) {
-    var reformattedCountry = {};
-    reformattedCountry.name = country.name;
     reformattedCountry.population = country.population;
-    return reformattedCountry;
-  });
-  return countryPopulations;
-};
-
-ApiProcessing.prototype.getCountryRegions = function(countries) {
-  var countryRegions = [];
-  countryRegions = countries.map(function(country) {
-    var reformattedCountry = {};
-    reformattedCountry.name = country.name;
     reformattedCountry.region = country.region;
-    return reformattedCountry;
-  });
-  return countryRegions;
-};
-
-ApiProcessing.prototype.getCountryBorders = function(countries) {
-  var countryBorders = [];
-  countryBorders = countries.map(function(country) {
-    var reformattedCountry = {};
-    reformattedCountry.name = country.name;
     reformattedCountry.borders = country.borders;
     return reformattedCountry;
   });
-  return countryBorders;
+  return countryStats;
 };
+
+// ApiProcessing.prototype.getCountryArea = function(countries) {
+//   var countryAreas = [];
+//   countryAreas = countries.map(function(country) {
+//     var reformattedCountry = {};
+//     reformattedCountry.name = country.name;
+//     reformattedCountry.area = country.area;
+//     return reformattedCountry;
+//   });
+//   return countryAreas;
+// };
+
+// ApiProcessing.prototype.getCountryPopulation = function(countries) {
+//   var countryPopulations = [];
+//   countryPopulations = countries.map(function(country) {
+//     var reformattedCountry = {};
+//     reformattedCountry.name = country.name;
+//     reformattedCountry.population = country.population;
+//     return reformattedCountry;
+//   });
+//   return countryPopulations;
+// };
+
+// ApiProcessing.prototype.getCountryRegions = function(countries) {
+//   var countryRegions = [];
+//   countryRegions = countries.map(function(country) {
+//     var reformattedCountry = {};
+//     reformattedCountry.name = country.name;
+//     reformattedCountry.region = country.region;
+//     return reformattedCountry;
+//   });
+//   return countryRegions;
+// };
+
+// ApiProcessing.prototype.getCountryBorders = function(countries) {
+//   var countryBorders = [];
+//   countryBorders = countries.map(function(country) {
+//     var reformattedCountry = {};
+//     reformattedCountry.name = country.name;
+//     reformattedCountry.borders = country.borders;
+//     return reformattedCountry;
+//   });
+//   return countryBorders;
+// };
 
 module.exports = ApiProcessing;
 
--- client/public/apiProcessing.js
+++ client/public/apiProcessing.js
@@ -9,48 +9,62 @@ ApiProcessing.prototype.getCountryNames = function(countries) {
   return countryNames;
 };
 
-ApiProcessing.prototype.getCountryArea = function(countries) {
-  var countryAreas = [];
-  countryAreas = countries.map(function(country) {
+ApiProcessing.prototype.processCountriesApi = function(countries) {
+  var countryStats = [];
+  countryStats = countries.map(function(country) {
     var reformattedCountry = {};
     reformattedCountry.name = country.name;
     reformattedCountry.area = country.area;
-    return reformattedCountry;
-  });
-  return countryAreas;
-};
-
-ApiProcessing.prototype.getCountryPopulation = function(countries) {
-  var countryPopulations = [];
-  countryPopulations = countries.map(function(country) {
-    var reformattedCountry = {};
-    reformattedCountry.name = country.name;
     reformattedCountry.population = country.population;
-    return reformattedCountry;
-  });
-  return countryPopulations;
-};
-
-ApiProcessing.prototype.getCountryRegions = function(countries) {
-  var countryRegions = [];
-  countryRegions = countries.map(function(country) {
-    var reformattedCountry = {};
-    reformattedCountry.name = country.name;
     reformattedCountry.region = country.region;
-    return reformattedCountry;
-  });
-  return countryRegions;
-};
-
-ApiProcessing.prototype.getCountryBorders = function(countries) {
-  var countryBorders = [];
-  countryBorders = countries.map(function(country) {
-    var reformattedCountry = {};
-    reformattedCountry.name = country.name;
     reformattedCountry.borders = country.borders;
     return reformattedCountry;
   });
-  return countryBorders;
+  return countryStats;
 };
+
+// ApiProcessing.prototype.getCountryArea = function(countries) {
+//   var countryAreas = [];
+//   countryAreas = countries.map(function(country) {
+//     var reformattedCountry = {};
+//     reformattedCountry.name = country.name;
+//     reformattedCountry.area = country.area;
+//     return reformattedCountry;
+//   });
+//   return countryAreas;
+// };
+
+// ApiProcessing.prototype.getCountryPopulation = function(countries) {
+//   var countryPopulations = [];
+//   countryPopulations = countries.map(function(country) {
+//     var reformattedCountry = {};
+//     reformattedCountry.name = country.name;
+//     reformattedCountry.population = country.population;
+//     return reformattedCountry;
+//   });
+//   return countryPopulations;
+// };
+
+// ApiProcessing.prototype.getCountryRegions = function(countries) {
+//   var countryRegions = [];
+//   countryRegions = countries.map(function(country) {
+//     var reformattedCountry = {};
+//     reformattedCountry.name = country.name;
+//     reformattedCountry.region = country.region;
+//     return reformattedCountry;
+//   });
+//   return countryRegions;
+// };
+
+// ApiProcessing.prototype.getCountryBorders = function(countries) {
+//   var countryBorders = [];
+//   countryBorders = countries.map(function(country) {
+//     var reformattedCountry = {};
+//     reformattedCountry.name = country.name;
+//     reformattedCountry.borders = country.borders;
+//     return reformattedCountry;
+//   });
+//   return countryBorders;
+// };
 
 module.exports = ApiProcessing;
